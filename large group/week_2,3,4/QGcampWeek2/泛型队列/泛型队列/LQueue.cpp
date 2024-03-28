b298f7b3c664c8d191c65b2746a8ca41231b4d02
Ender39831 <719724460@qq.com>
2024-03-28 21:30:30 +0800

    格式修正

---
+++ large group/week_2,3,4/QGcampWeek2/泛型队列/泛型队列/LQueue.cpp
@@ -0,0 +1,162 @@
+﻿#include "LQueue.h"
+#include <iostream>
+#include <any>
+#include <string>
+
+Node::Node(std::any value) : data(value), next(nullptr) {}
+
+	LQueue::LQueue() : front(nullptr), rear(nullptr),length(0) {}
+	// 添加结点函数
+	void LQueue::addNode() {
+		std::string input;
+		std::any value;
+		std::cout << "请输入一个数据(可以是字符串，浮点数，整数)：";
+		std::cin >> input;
+
+		if (isInteger(input)) {    //是否为整数
+			int intValue = std::stoi(input);
+			value = intValue;
+		}
+		else if (isFloat(input)) {  //是否为浮点数
+			double doubleValue = std::stod(input);
+			value = doubleValue;
+		}
+		else        // 作为字符串处理
+		{
+			value = input;
+		}
+
+		Node* newNode = new Node(value);
+		if (rear == nullptr) {
+			rear = newNode;
+			front = newNode;
+		}
+		else {
+			rear->next = newNode;
+			rear = rear->next;
+		}
+
+		std::cout << "结点添加完成!" << std::endl;
+		length++; //记录队列长度
+	}
+	// 删除结点函数
+	void LQueue::deleteNode() {
+		if (front == nullptr) {
+			std::cout << "队列为空，无法删除结点！" << std::endl;
+			return;
+		}
+
+		Node* temp = front;
+		front = front->next;
+		delete temp;
+
+		if (front == nullptr) {
+			rear = nullptr;
+		}
+	
+		std::cout << "删除结点完成!" << std::endl;
+		length--;
+	}
+	// 判断队列是否为空
+	void LQueue::IsEmptyLQueue() {
+		if (front == nullptr) {
+			std::cout << "队列为空!" << std::endl;
+		}
+		else {
+			std::cout << "队列不为空!" << std::endl;
+		}
+	}
+	// 清空队列
+	void LQueue::ClearLQueue() {
+		while (front != nullptr) {
+			deleteNode();
+			front = front->next;
+		}
+		length = 0;
+	}
+	// 返回队列长度
+	int LQueue::LengthLQueue() {
+		return length;
+	}
+	// 打印队列
+	void LQueue::Print() {
+		if (front == nullptr) {
+			std::cout << "队列为空" << std::endl;
+			return;
+		}
+
+		std::cout << std::endl<<"队列中的结点为：" << std::endl;
+		Node* current = front;
+		while (current != nullptr) {
+			// 判断数据类型是啥并输出
+			try {
+				if (current->data.type() == typeid(int)) {
+					std::cout << std::any_cast<int>(current->data) << std::endl;
+				}
+				else if (current->data.type() == typeid(double)) {
+					std::cout << std::any_cast<double>(current->data) << std::endl;
+				}
+				else if (current->data.type() == typeid(std::string)) {
+					std::cout << std::any_cast<std::string>(current->data) << std::endl;
+				}
+			}
+			catch (const std::bad_any_cast&) {   //错误判断
+				std::cout << "无法解析数据类型!";
+			}
+			current = current->next;
+		}
+		std::cout << std::endl;
+	}
+	//判断是否为整数
+	bool isInteger(const std::string& s) {
+		if (s.empty()) {
+			return false;
+		}
+
+		size_t start = 0;
+		if (s[0] == '-' || s[0] == '+') {
+			start = 1;
+		}
+
+		for (size_t i = start; i < s.length(); i++) {
+			if (!isdigit(s[i])) {
+				return false;
+			}
+		}
+
+		if (start == 1 && s.length() == 1) {
+			return false; 
+		}
+
+		return true;
+	}
+	// 判断是否为浮点数
+	bool isFloat(const std::string& s) {
+		if (s.empty()) {
+			return false;
+		}
+
+		size_t start = 0;
+		if (s[0] == '-' || s[0] == '+') {
+			start = 1;
+		}
+
+		bool sign = false;
+
+		for (size_t i = start; i < s.length(); i++) {
+			if (!isdigit(s[i])) {
+				if (s[i] == '.' && !sign) {
+					sign = true;
+				}
+				else {
+					return false;
+				}
+			}
+		}
+
+		if (start == 1 && s.length() == 1) {
+			return false; 
+		}
+
+		return sign;
+	}
---
+++ large group/week_2,3,4/QGcampWeek2/链栈/main.cpp
@@ -0,0 +1,59 @@
+﻿#include "ChainStack.h"
+#include <iostream>
+#include <string>
+
+int main() {
+
+    ChainStack Stack;
+    char choice;
+    std::string expression;  //存储用于计算的表达式
+
+    while (true) {
+        std::cout << std::endl << "链栈操作菜单" << std::endl;
+        std::cout << "1.添加节点" << std::endl;
+        std::cout << "2.删除节点" << std::endl;
+        std::cout << "3.判断链栈是否为空" << std::endl;
+        std::cout << "4.清空链栈" << std::endl;
+        std::cout << "5.计算模式，启动" << std::endl;
+        std::cout << "6.打印链栈中的数据" << std::endl;
+        std::cout << "7.结束程序" << std::endl;
+        std::cout << "请选择操作:";
+        std::cin >> choice;
+
+        switch (choice) {
+        case '1':
+            std::cout << "输入你想要输入的数据(字符):";
+            char t;
+            std::cin >> t;
+            Stack.push(t);
+            break;
+        case '2':
+            Stack.pop();
+            break;
+        case '3':
+            if (Stack.isEmpty())
+                std::cout << "栈为空" << std::endl;
+            else
+                std::cout << "栈不为空" << std::endl;
+            break;
+        case '4':
+            Stack.clearStack();
+            break;
+        case '5':
+            std::cout << "请输入要计算的四则运算表达式：";
+            std::cin >> expression;
+            std::cout << "计算结果为：" << evaluateExpression(expression,Stack) << std::endl;
+            break;
+        case '6':
+            Stack.printStack();
+            break;
+        case '7':
+            std::cout << "程序退出！" << std::endl;
+            return 0;
+        default:
+            std::cout << "无效操作!" << std::endl;
+        }
+    }
+
+    return 0;
+}
